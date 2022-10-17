9f0897d2b760b99eaeff1062c3b8fa03652c0fe8
Enigma406 <idrisoyinpopoola@gmail.com>
2022-10-17 00:02:51 -0700

    0x01.C-variables_if_else_while Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Description: program that prints all single digit numbers of base
+ * Return: 0
+*/
+
+int main(void)
+{
+	int a;
+
+	(a = 0);
+	while (a < 10)
+	if (a < 10)
+	putchar(a);
+	a++;
+	putchar('\n');
+	return (0);
+
+}
