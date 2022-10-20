383c95586c808d8133524c11fc477e6f11d5b4db
Enigma406 <idrisoyinpopoola@gmail.com>
2022-10-19 23:07:54 -0700

    0x02. C - Functions, nested loops Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,13 @@
 
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int ld;
 
-	if (ld < 0)
-	ld = -1;
-
+	if (n < 0)
+	n = -n;
+	ld = n % 10;
 	_putchar(ld + '0');
 
-	return (0);
+	return (n % 10);
 
 }
