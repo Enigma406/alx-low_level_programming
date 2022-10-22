ad968b97ff0d1404cd55a6faf6c55b01e7622240
Enigma406 <idrisoyinpopoola@gmail.com>
2022-10-22 04:38:17 -0700

    0x04. C - More Functions, nested loops Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,17 +6,16 @@
  */
 void print_diagonal(int n)
 {
-	int x, i;
-
 	if (n <= 0)
 	_putchar('\n');
 else
 {
+	int x, i;
+
 	for (x = 0; x < n; x++)
 {
 	for (i = 0, i < x; i++)
-	_putchar(' ');
-	_putchar(92);
+	_putchar('\');
 	_putchar('\n');
 }
 }
