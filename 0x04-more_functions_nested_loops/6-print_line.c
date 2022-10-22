60c7c56a53de38dc171d9b4308c4ddaede4bda1d
Enigma406 <idrisoyinpopoola@gmail.com>
2022-10-21 23:28:21 -0700

    0x04. C - More Functions, nested loops Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n: the number of '_' to be printed
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int lngth;
+
+	if (n > 0)
+{
+	for (lngth = 0; lngth < 0; lngth++)
+	_putchar('_');
+}
+	_putchar('\n');
+
+}
