1094f2e3885fe704aa2c0ecdba257deca24445f0
Enigma406 <idrisoyinpopoola@gmail.com>
2022-10-22 00:13:12 -0700

    0x04. C - More Functions, nested loops Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,14 +10,12 @@ void print_line(int n)
 {
 	int l;
 
-	if (n > 0)
+	if (n <= 0)
+	_putchar('\n');
+	else
 {
 	for (l = 0; l < n; l++)
 	_putchar('_');
-}
-	else
-{
-	_putchar('\n');
 }
 	_putchar('\n');
 
