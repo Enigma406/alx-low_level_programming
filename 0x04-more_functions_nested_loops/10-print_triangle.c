a5e06c751e8953cdd26e5fa28967d1f59d1700d2
Enigma406 <idrisoyinpopoola@gmail.com>
2022-10-23 22:51:14 -0700

    0x04. C - More Functions, nested loops Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+{
+	_putchar('\n');
+}
+	else
+{
+	int a, b, c;
+
+	for (a = 0; a < size; a++)
+{
+	for (b = 0; b < size; b++)
+{
+	for (c = 0; c < size; c++)
+{
+	_putchar('#');
+}
+	_putchar('\n');
+}
+}
+}
+}
