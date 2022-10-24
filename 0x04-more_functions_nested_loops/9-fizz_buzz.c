5501973d7e2001c1a1bdaa574ca8ff3bb2d883ad
Enigma406 <idrisoyinpopoola@gmail.com>
2022-10-24 00:33:40 -0700

    0x04. C - More Functions, nested loops Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,27 +1,28 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - prints numbers with fizz_buzz.
  * Return: 0
  */
 int main(void)
 {
-
 	int n;
 
-	printf("1");
-	printf(" ");
+	printf("1 ");
 	for (n = 2; n <= 100; n++)
 {
-	if (n % 3 == 0)
+	if (n % 3 == 0 && n % 5 == 0)
+	printf("FizzBuzz ");
+	else if (n % 3 == 0)
 	printf("Fizz ");
 	else if (n % 5 == 0)
 	printf("Buzz ");
-	else if (n % 3 == 0 && n % 5 == 0)
-	printf("FizzBuzz ");
 	else
 	printf("%d ", n);
 }
 	printf("\n");
 	return (0);
+
 }
+
